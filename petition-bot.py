c5877bac7787820c898a6ecf543a3b99825e3126
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-10-23 09:59:54 -0300

    some advancments

--- petition-bot.py
+++ petition-bot.py
@@ -10,6 +10,8 @@ from datetime import datetime
 from random import randint, uniform
 from time import sleep, time
 
+import numpy as np
+import scipy.interpolate as si
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
@@ -190,29 +192,101 @@ class petitionBot(unittest.TestCase):
 
         self.log('COLORS OK')
 
+    # Using B-spline for simulate humane like mouse movments
+    def human_like_mouse_move(self, action, start_element):
+        points = [[6, 2], [3, 2], [0, 0], [0, 2]]
+        points = np.array(points)
+        x = points[:, 0]
+        y = points[:, 1]
+
+        t = range(len(points))
+        ipl_t = np.linspace(0.0, len(points) - 1, 100)
+
+        x_tup = si.splrep(t, x, k=1)
+        y_tup = si.splrep(t, y, k=1)
+
+        x_list = list(x_tup)
+        xl = x.tolist()
+        x_list[1] = xl + [0.0, 0.0, 0.0, 0.0]
+
+        y_list = list(y_tup)
+        yl = y.tolist()
+        y_list[1] = yl + [0.0, 0.0, 0.0, 0.0]
+
+        x_i = si.splev(ipl_t, x_list)
+        y_i = si.splev(ipl_t, y_list)
+
+        startElement = start_element
+
+        action.move_to_element(startElement)
+        action.perform()
+
+        c = 5  # change it for more move
+        i = 0
+        for mouse_x, mouse_y in zip(x_i, y_i):
+            action.move_by_offset(mouse_x, mouse_y)
+            action.perform()
+            self.log("Move mouse to, %s ,%s" % (mouse_x, mouse_y))
+            i += 1
+            if i == c:
+                break
+
     def test_run(self):
-        htmlBody = '/html/body/form/div[4]'
-        firstButton = '/html/body/form/div[4]/div/table/tbody/tr/td[2]/div[2]/a'
+        action = ActionChains(self.driver)
         nameInput = '//*[@id="ctl00_cmain_txtNome"]'
         emailInput = '//*[@id="ctl00_cmain_txtEmail"]'
         termsInput = '//*[@id="ctl00_cmain_chkAutorizoContacto"]'
-        lastButton = '//*[@id="ctl00_cmain_cmdSign"]'
+        submitButton = '//*[@id="ctl00_cmain_cmdSign"]'
+        version = 5
 
-        # for i in range(2000):
         self.driver.get(
             'https://peticaopublica.com.br/psign.aspx?pi=BR120937')
 
         self.log('Wait')
         self.wait_between(MIN_RAND, MAX_RAND)
-        # self.driver.find_element_by_xpath(
-        #     nameInput).send_keys('username {}'.format(i))
-        # sleep(1.5)
-        # self.driver.find_element_by_xpath(
-        #     emailInput).send_keys('username1{}@gmail.com'.format(i))
-        # sleep(1.5)
-        # self.driver.find_element_by_xpath(termsInput).click()
-        # sleep(1.5)
-        # self.driver.find_element_by_xpath(lastButton).click()
+
+        self.log('Name input')
+        nameInputElement = self.driver.find_element_by_xpath(nameInput)
+        self.human_like_mouse_move(action, nameInputElement)
+        nameInputElement.send_keys('username {}'.format(version))
+
+        self.log('Wait')
+        self.wait_between(MIN_RAND, MAX_RAND)
+
+        emailInputElement = self.driver.find_element_by_xpath(emailInput)
+        self.human_like_mouse_move(action, emailInputElement)
+        emailInputElement.send_keys('username{}new@gmail.com'.format(version))
+
+        self.log('Wait')
+        self.wait_between(MIN_RAND, MAX_RAND)
+
+        termsInputElement = self.driver.find_element_by_xpath(termsInput)
+        self.human_like_mouse_move(action, termsInputElement)
+        termsInputElement.click()
+
+        self.log('Wait')
+        self.wait_between(MIN_RAND, MAX_RAND)
+
+        submitButtonElement = self.driver.find_element_by_xpath(submitButton)
+        self.human_like_mouse_move(action, submitButtonElement)
+        submitButtonElement.click()
+
+        self.log('Wait')
+        self.wait_between(MIN_RAND, MAX_RAND)
+
+        for j in range(2000):
+            self.driver.back()
+            emailInputElement = self.driver.find_element_by_xpath(emailInput)
+            self.human_like_mouse_move(action, emailInputElement)
+            emailInputElement.clear()
+            emailInputElement.send_keys(
+                'username{}{}@gmail.com'.format(version, j))
+            self.log('Wait')
+            self.wait_between(MIN_RAND, MAX_RAND)
+            submitButtonElement = self.driver.find_element_by_xpath(
+                submitButton)
+            self.human_like_mouse_move(action, submitButtonElement)
+            submitButtonElement.click()
 
     def tearDown(self):
         self.log('Ending bot...')
